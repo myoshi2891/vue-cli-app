505b04bff7870723d826af2f550de729eac51138
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-02-09 03:17:31 +0900

    A Potential Problem

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import FriendContact from "./components/FriendContact.vue";
-import NewFriend from "./components/NewFriend.vue";
+import { createApp } from 'vue';
 
-const app = createApp(App).mount("#app");
+import App from './App.vue';
+import ActiveElement from './components/ActiveElement.vue';
+import KnowledgeBase from './components/KnowledgeBase.vue';
+import KnowledgeElement from './components/KnowledgeElement.vue';
+import KnowledgeGrid from './components/KnowledgeGrid.vue';
 
-app.component("friend-contact", FriendContact);
-app.component("new-friend", NewFriend);
+const app = createApp(App);
 
-app.mount("#app");
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
+
+app.mount('#app');
