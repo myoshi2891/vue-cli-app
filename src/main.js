590fe1f4c08b8dc691b03af69404c8dd3c926e7e
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-02-10 12:56:50 +0900

    Global vs Local Components

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import ActiveElement from './components/ActiveElement.vue';
-import KnowledgeBase from './components/KnowledgeBase.vue';
-import KnowledgeElement from './components/KnowledgeElement.vue';
-import KnowledgeGrid from './components/KnowledgeGrid.vue';
+import BaseBadge from "./components/BaseBadge.vue";
 
 const app = createApp(App);
 
-app.component('active-element', ActiveElement);
-app.component('knowledge-base', KnowledgeBase);
-app.component('knowledge-element', KnowledgeElement);
-app.component('knowledge-grid', KnowledgeGrid);
+app.component("base-badge", BaseBadge);
 
 app.mount('#app');
