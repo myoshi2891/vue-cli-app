49d913055c42af4642da8c564bb5cf2536e24c78
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-02-11 14:36:36 +0900

    Setup & First Steps

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
 <template>
-  <div>
-    <the-header></the-header>
-
-    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
-    <!-- <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
-    <keep-alive>
-      <component :is="selectedComponent"></component>
-    </keep-alive>
-  </div>
+  <ul></ul>
 </template>
 
 <script>
-import TheHeader from './components/layout/TheHeader.vue';
-// import BadgeList from "./components/BadgeList.vue";
-// import UserInfo from "./components/UserInfo.vue";
-// import CourseGoals from './components/CourseGoals.vue';
-import ActiveGoals from './components/ActiveGoals.vue';
-import ManageGoals from './components/ManageGoals.vue';
-
 export default {
-  components: {
-    TheHeader,
-    ActiveGoals,
-    ManageGoals
-  },
   data() {
     return {
-      selectedComponent: 'active-goals',
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-  methods: {
-    setSelectedComponent(cmp) {
-      this.selectedComponent = cmp;
+      storedResources: [
+        {
+          id: 'official-guide', title: 'Official Guide', description: 'The official Vue.js documentation',
+          link: 'https://vue.js.org'
+        },
+        {
+          id: 'google', title: 'Google', description: 'Learn to google',
+          link: 'https://google.org'
+        },
+        ]
+      }
     }
-  }
-};
+  } 
 </script>
 
 <style>
