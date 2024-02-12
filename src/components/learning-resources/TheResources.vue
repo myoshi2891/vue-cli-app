05a28330eedc8eb3257aea000498bc460d358b58
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-02-12 13:09:24 +0900

    Deleting Items

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -36,6 +36,7 @@ export default {
         return {
             resources: this.storedResources,
             addResource: this.addResource,
+            deleteResource: this.removeResource
         };
     },
     computed: {
@@ -59,6 +60,10 @@ export default {
             };
             this.storedResources.unshift(newResource);
             this.selectedTab = 'stored-resources';
+        },
+        removeResource(resId) {
+            const resIndex = this.storedResources.findIndex(res => res.id === resId)
+            this.storedResources.splice(resIndex, 1)
         }
     }
 }
